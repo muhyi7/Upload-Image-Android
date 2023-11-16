2c7a1850b80940e21b38589caf4c8e2aaa770b46
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-16 16:09:59 +0700

    success upload with custom directory

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 if(!empty($_POST['image'])){
-    if(file_put_contents(date("d-m-y").'-'.time().'-'.rand(10000, 10000). '.jpg',
+    if(file_put_contents('images/'.date("d-m-y").'-'.time().'-'.rand(10000, 10000). '.jpg',
     base64_decode($_POST['image']))){
         echo 'success';
     }
