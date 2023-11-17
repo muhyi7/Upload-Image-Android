e2ed353640d48587bcdfafc3da1b1029f57cf3ee
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-17 07:29:12 +0700

    finish upload path to database

--- upload.php
+++ upload.php
@@ -1,9 +1,20 @@
 <?php
+$con = mysqli_connect("localhost", "root", "", "upload_image");
+if($con){
+
 if(!empty($_POST['image'])){
-    if(file_put_contents('images/'.date("d-m-y").'-'.time().'-'.rand(10000, 10000). '.jpg',
+    $path = 'images/'.date("d-m-y").'-'.time().'-'.rand(10000, 10000). '.jpg';
+    if(file_put_contents($path,
     base64_decode($_POST['image']))){
+    $sql = "insert into images(path) values ('".$path."')";
+    if(mysqli_query($con, $sql)){
         echo 'success';
+    }
+    else echo 'Failed to insert to Database';
     }
     else echo 'Failed to upload image';
 }
-else echo 'No image found';
+else echo 'No image found';   
+}
+else echo "Database connection failed";
+?>
